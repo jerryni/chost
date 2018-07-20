b34cb6c3bb40ebf551a059b3f7fbf71b5175e0e7
nirizhe <nirizhe@youzan.com>
2018-07-20 21:16:47 +0800

    fix: window os host path

--- src/constant.js
+++ src/constant.js
@@ -2,7 +2,7 @@ let HOST_PATH,
     isWin = /^win/.test(process.platform)
 
 if(isWin){
-    HOST_PATH = 'C:\Windows\System32\drivers\etc'
+    HOST_PATH = 'C:/Windows/System32/drivers/etc/hosts'
 } else {
     HOST_PATH = '/private/etc/hosts'
 }
